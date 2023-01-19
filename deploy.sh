24b2a60580d7758a9e9a88c82bed2faa269e0198
Javier Epifanio <javi.epi10@gmail.com>
2023-01-19 21:53:53 +0100

    deploy

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:javiepi/javiepi.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:javiepi/Tic-tac-toe.git main:gh-pages
